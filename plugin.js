0822afa43dc2f87d57296963cfbc72d762fd4ac8
grabowCommuter <grabow.commuter@gmail.com>
2019-01-22 16:12:23 +0100

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,18 @@
+{
+  "moduleId" : "grabowCommuter/TomTomMap",
+  "title" : "TomTom Map",
+  "subtitle" : "Traffic on TomTomMaps",
+
+  "backButton" : true,
+  "enableGPS" : true,
+  "zoomControl" : false,
+  "screenLockRot" : false,
+  "reload" : true,
+
+  "shouldOverrideUrlLoading1" : "http",
+  "shouldOverrideUrlLoading2" : "https",
+
+  "loadUrl" : "https://web-commuter.firebaseapp.com/tomtom2.html?lat=#lat#&lng=#lng#&zoom=#zoom#",
+
+  "browserLaunchLink" : "https://web-commuter.firebaseapp.com/tomtom2.html?lat=#lat#&lng=#lng#&zoom=#zoom#"
+}
